e62811cdd1b436b8e7d9f9daf2b2238bfd7c32fb
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2020-08-12 12:28:32 +0100

    front end

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  # has_many :review
+  # validate :name, presence: true
+  # validate :address, presence: true
+  # validates_inclusion_of :category, in: ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  # has_many :review
+end
---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  # validates :content, presence: true
+  # validates :rating, inclusion: 1..5
+  # belongs_to :restraunt
+end
